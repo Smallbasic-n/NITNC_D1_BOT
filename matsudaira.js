3db172cfedb50460eee493a0671038d4a68b34b4
SmallBasic-n <ntppc@outlook.jp>
2024-11-02 07:27:24 +0900

    ロール付与機能ができた

--- matsudaira.js
+++ matsudaira.js
@@ -1,5 +1,5 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits, Collection, REST, Routes, SlashCommandBuilder, ChatInputCommandInteraction } = require('discord.js');
+const { Client, Events, GatewayIntentBits, Collection, REST, Routes, SlashCommandBuilder, ChatInputCommandInteraction, PermissionsBitField,Partials } = require('discord.js');
 const { token, guildId, clientId, passphrase, salt } = require('./matsudaira.json');
 const iv= Buffer.from('00000000000000000000000000000000', 'hex');
 const fs = require('node:fs')
@@ -24,12 +24,17 @@ var read = () => {//decrypt
   return dec;
 }
 
+
 var userData={users: [{studentId: 'Z99999', givenName: 'TEST', firstName: 'TESTER', rohmeFirstName: 'TESUTA', rohmeGivenName: 'TESUTO', accountId: 'INVAILD'}],roles: []}
 //if (!fs.existsSync('./user.iv')) {fs.writeFileSync("./user.iv",crypto.randomBytes(16))}
 if (!fs.existsSync('./user.data')) {write(JSON.stringify(userData))}
 userData=JSON.parse(read());
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions],
+	partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+    disableEveryone: false
+});
 
 const commands = [];
 client.commands = new Collection();
@@ -74,7 +79,6 @@ const iam_command={
         await interaction.reply("<@"+accountId+"> の中の人\n 名前："+givenname+"　"+firstname+"　("+rohmegivenname+" "+rohmefirstname+")\n学籍番号："+studentid);
     },
 };
-
 const whois_command={
     data: new SlashCommandBuilder()
         .setName('whois')
@@ -94,9 +98,166 @@ const whois_command={
         await interaction.reply({ content: "@"+user.username+" の中の人\n 名前："+data.givenName+"　"+data.firstName+"　("+data.rohmeGivenName+" "+data.rohmeFirstName+")\n学籍番号："+data.studentId, ephemeral: true});
     },
 };
+const role_command={
+    data: new SlashCommandBuilder()
+        .setName('create')
+        .setDescription('松平定信公にロールを作成してもらいます．')
+        .addStringOption(option=>option.setName("name").setDescription("ロール名").setRequired(true))
+        ,
+    /**
+     * 
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+    async execute(interaction) {
+        const user=interaction.user;
+        const role=interaction.options.getString("name");
+        const members=interaction.guild.members.cache.filter(member=>member.permissions.has('ManageRoles')).find((val)=>val.id==user.id);
+        if (members==undefined){
+            await interaction.reply({ content: "あなたの権限が不足しています．", ephemeral: true});return;
+        }
+        var id=(await interaction.guild.roles.create({
+                    name: role,
+                    color: [0,0,255],
+                    reason: '松平定信公により作成されました．',
+                    permissions: [PermissionsBitField.Flags.SendMessages, PermissionsBitField.Flags.ViewChannel, PermissionsBitField.Flags.MentionEveryone,PermissionsBitField.Flags.UseExternalEmojis,PermissionsBitField.Flags.AddReactions,PermissionsBitField.Flags.CreatePublicThreads,PermissionsBitField.Default,PermissionsBitField.Flags.CreatePublicThreads],
+                    mentionable: true
+
+            })).id;
+        //if (userData.roles.find((val)=>val=id)==undefined){
+            userData.roles.push(id);
+        //}
+
+        write(JSON.stringify(userData));
+        await interaction.reply({ content: "次のロールを作成し，松平定信公の管理下に置きました．\nロール名：<@&"+id+">", ephemeral: true});
+    },
+};
+const role_add_command={
+    data: new SlashCommandBuilder()
+        .setName('add')
+        .setDescription('ロールを松平定信公の管理下に置きます．')
+        .addRoleOption((role)=>role.setName("role").setDescription("松平定信公の管理下に置きたいロール").setRequired(true))
+        ,
+    /**
+     * 
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+    async execute(interaction) {
+        const user=interaction.user;
+        const role=interaction.options.getRole("role");
+        const members=interaction.guild.members.cache.filter(member=>member.permissions.has('ManageRoles')).find((val)=>val.id==user.id);
+        if (members==undefined){
+            await interaction.reply({ content: "あなたの権限が不足しています．", ephemeral: true});return;
+        }
+        var find=(await interaction.guild.roles.fetch()).find((rl)=>rl.id=role.id)
+        //if (find==undefined){
+        //    await interaction.guild.roles.create(role);
+        //    find=(await interaction.guild.roles.fetch()).find((rl)=>rl.id=role.id)
+        //}
+        //if (userData.roles.find((val)=>val=find.id)==undefined){
+            userData.roles.push(find.id);
+        //}
+
+        write(JSON.stringify(userData));
+        await interaction.reply({ content: "次のロールを松平定信公の管理下に置きました．\nロール名：<@&"+find.id+">", ephemeral: true});
+    },
+};
+const role_list_command={
+    data: new SlashCommandBuilder()
+        .setName('list')
+        .setDescription('松平定信公の管理下に置かれているロールを表示します．')
+        ,
+    /**
+     * 
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+    async execute(interaction) {
+        const user=interaction.user;
+        const members=interaction.guild.members.cache.filter(member=>member.permissions.has('ManageRoles')).find((val)=>val.id==user.id);
+        if (members==undefined){
+            await interaction.reply({ content: "あなたの権限が不足しています．", ephemeral: true});return;
+        }
+        var data="";
+        userData.roles.forEach(element => {
+            data=data+"\n<@&"+element+"> (ID:"+element+")"
+        });
+        await interaction.reply({ content: "次のロールは松平定信公の管理下に置かれています．"+data, ephemeral: true});
+    },
+};
+const role_del_command={
+    data: new SlashCommandBuilder()
+        .setName('del')
+        .setDescription('ロールを松平定信公の管理下からはずします．')
+        .addRoleOption((role)=>role.setName("role").setDescription("松平定信公の管理下より外したいロール").setRequired(false))
+        .addStringOption((rolei)=>rolei.setName("roleint").setDescription("松平定信公の管理下より外したいロールId(すでに削除されたロールに使用)").setRequired(false))
+        
+        ,
+    /**
+     * 
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+    async execute(interaction) {
+        const user=interaction.user;
+        const role=interaction.options.getRole("role");
+        const rolei=(interaction.options.getString("roleint"));
+        if (role == undefined&&rolei==undefined){
+            await interaction.reply({ content: "ロール名を指定してください．", ephemeral: true});return;
+        }
+        const members=interaction.guild.members.cache.filter(member=>member.permissions.has('ManageRoles')).find((val)=>val.id==user.id);
+        if (members==undefined){
+            await interaction.reply({ content: "あなたの権限が不足しています．", ephemeral: true});return;
+        }
+        const id=role == undefined? rolei:role.id
+        const index=userData.roles.findIndex((val)=>val=id)
+        if (index == -1){
+            await interaction.reply({ content: "そのロールは松平定信公の管理下に置かれていません．", ephemeral: true});return;
+        }
+        userData.roles.splice(index,1);
+        write(JSON.stringify(userData));
+        await interaction.reply({ content: "次のロールを松平定信公の管理下から外しました．\nロール名：<@&"+id+"> (ID:"+id+")", ephemeral: true});
+    },
+};
+const study_command={
+    data: new SlashCommandBuilder()
+        .setName('study')
+        .setDescription('松平定信公によるロール付与令を発行します．')
+        ,
+    /**
+     * 
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+    async execute(interaction) {
+        const user=interaction.user;
+        const members=interaction.guild.members.cache.filter(member=>member.permissions.has('ManageRoles')).find((val)=>val.id==user.id);
+        if (members==undefined){
+            await interaction.reply({ content: "あなたの権限が不足しています．", ephemeral: true});return;
+        }
+        var explain=""
+        var emojis=[]
+        var index=1
+        userData.roles.forEach(element => {
+            var nowemoji=String.fromCodePoint(0x1F600 + index);
+            emojis.push(nowemoji);
+            explain+="\n"+nowemoji+"：<@&"+element+">"
+            index+=1;
+        });
+        await interaction.reply({ content: "事前審査を通過しました．", ephemeral: true , });
+        var message=await interaction.guild.channels.cache.get(interaction.channelId).send({content: '松平定信公によるロール付与の令\n @everyone ，自分が該当するロールにリアクションをつけてください．'+explain})
+        //console.log(message)
+        //interaction.guild.channels.cache.get(interaction.channelId).lastMessage
+        emojis.forEach(elem=>{
+            message.react(elem);
+        })
+        
+    },
+};
 
 commands.push(iam_command.data.toJSON());
 commands.push(whois_command.data.toJSON());
+commands.push(role_command.data.toJSON());
+commands.push(role_add_command.data.toJSON());
+commands.push(role_list_command.data.toJSON());
+commands.push(role_del_command.data.toJSON());
+commands.push(study_command.data.toJSON());
 
 const rest = new REST().setToken(token);
 
@@ -119,6 +280,11 @@ const rest = new REST().setToken(token);
 
 client.commands.set(iam_command.data.name, iam_command);
 client.commands.set(whois_command.data.name, whois_command);
+client.commands.set(role_command.data.name, role_command);
+client.commands.set(role_add_command.data.name, role_add_command);
+client.commands.set(role_list_command.data.name, role_list_command);
+client.commands.set(role_del_command.data.name, role_del_command);
+client.commands.set(study_command.data.name, study_command);
 
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
@@ -147,5 +313,19 @@ client.once(Events.ClientReady,async readyClient => {
     member.setNickname('松平定信').catch(console.error);
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
-
+client.on(Events.MessageReactionAdd,  async(reaction,user)=>{
+    const message = reaction.message
+   const member = message.guild.members.resolve(user)
+   var ok=false
+   var index=1
+   userData.roles.forEach(element => {
+        if (ok){return;}
+       var nowemoji=String.fromCodePoint(0x1F600 + index);
+       if (reaction.emoji.name === nowemoji){
+            member.roles.add(message.guild.roles.cache.find(role=>role.id == element))
+            ok=true;
+       }
+       index+=1
+   });
+})
 client.login(token);

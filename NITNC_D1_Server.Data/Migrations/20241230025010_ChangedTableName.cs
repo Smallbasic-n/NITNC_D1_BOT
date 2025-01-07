9ecff91ecde8484c33f1bb88c311e29831445d26
SmallBasic-n <ntppc@outlook.jp>
2025-01-07 15:18:16 +0900

    フォルダ構成を変更した

---
+++ NITNC_D1_Server.Data/Migrations/20241230025010_ChangedTableName.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NITNC_D1_Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedTableName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MatsudairaROles",
+                table: "MatsudairaROles");
+
+            migrationBuilder.RenameTable(
+                name: "MatsudairaROles",
+                newName: "MatsudairaRoles");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MatsudairaRoles",
+                table: "MatsudairaRoles",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MatsudairaRoles",
+                table: "MatsudairaRoles");
+
+            migrationBuilder.RenameTable(
+                name: "MatsudairaRoles",
+                newName: "MatsudairaROles");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MatsudairaROles",
+                table: "MatsudairaROles",
+                column: "Id");
+        }
+    }
+}
---
+++ NITNC_D1_Server.Data/Migrations/20241230025351_ChangedColumnName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NITNC_D1_Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedColumnName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RoleId",
+                table: "MatsudairaRoles",
+                newName: "DiscordRoleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DiscordRoleId",
+                table: "MatsudairaRoles",
+                newName: "RoleId");
+        }
+    }
+}
